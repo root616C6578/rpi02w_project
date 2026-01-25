c7a1d5ea299c759f508d2fc7317c53da91837f72
root616C6578 <vitrivalexander@gmail.com>
2026-01-25 15:41:20 +0200

    oiuoi

--- src/rpi433.py
+++ src/rpi433.py
@@ -65,7 +65,7 @@ def rpi433_menu(disp, original_img, BUTTON_UP, BUTTON_DOWN, BUTTON_SELECT):
                 # Receive Loop
                 while True:
                     timestamp = rf_rx.rx_code_timestamp
-                    while rf_rx.rx_code_timestamp == timestamp:
+                    #while rf_rx.rx_code_timestamp == timestamp:
                         # Вихід по кнопці SELECT
                         #if GPIO.input(BUTTON_SELECT) == GPIO.LOW:
                         #    return
